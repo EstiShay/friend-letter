a81b2cb3188ea5207c07d80ebd9fd21158bc9960
Evan Johnson and Esti Shay <esti@>
2017-08-07 09:55:44 -0700

    refactor using Handlebars layout template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,44 +1,23 @@
 import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
 
         staticFileLocation("/public");
 
-        get("/", (request, response) -> "<!DOCTYPE html>" +
-                "<html>" +
-                "<head>" +
-                "<title>Hello Friend!</title>" +
-                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                "</head>" +
-                "<body>" +
-                "<h1>Hello From Afar</h1>" +
-                "<p>Dear Friend,</p>" +
-                "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                "<p>Cheers,</p>" +
-                "<p>Travel Enthusiast Jane</p>" +
-                "</body>" +
-                "</html>"
-        );
-        //new route below
-        get("/favorite_photos", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Traveling Photos</h1>" +
-                        "<ul>" +
-                        "<li><img src='/images/city-lights.jpeg' alt='A photo of city lights.'/></li>" +
-                        "<li><img src='/images/snowy-roads.jpeg' alt='A photo of snow roads.'/></li>" +
-                        "</ul>" +
-                        "</body>" +
-                        "</html>"
-        );
+        get("/", (request, response) -> {
+            Map<String, Object>model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hello.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/favorite_photos", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
